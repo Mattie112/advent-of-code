467205cc08c949663bb7d3f7239c6d63de3dfce3
Matthijs van Schooten <matthijs@munisense.com>
2022-12-17 10:59:39 +0100

    Removed useless code for day4

--- 2022/day4/day4.go
+++ 2022/day4/day4.go
@@ -28,25 +28,15 @@ func day4Part1(path string) int {
 		matchesInt := utils.StringSliceToIntSlice(matches[1:]) // First match (the line) cannot be parsed
 		matchesInt = append([]int{-1}, matchesInt...)          // To make it easier I re-add the first element
 
-		first := expandInts(matchesInt[re.SubexpIndex("firstFrom")], matchesInt[re.SubexpIndex("firstTo")])
-		second := expandInts(matchesInt[re.SubexpIndex("secondFrom")], matchesInt[re.SubexpIndex("secondTo")])
-
-		// Find total overlaps
-		if second[0] >= first[0] && second[len(second)-1] <= first[len(first)-1] || first[0] >= second[0] && first[len(first)-1] <= second[len(second)-1] {
+		// Find total overlaps (split into 2 for readability)
+		if matchesInt[re.SubexpIndex("secondFrom")] >= matchesInt[re.SubexpIndex("firstFrom")] && matchesInt[re.SubexpIndex("secondTo")] <= matchesInt[re.SubexpIndex("firstTo")] {
+			overlapCount++
+		} else if matchesInt[re.SubexpIndex("firstFrom")] >= matchesInt[re.SubexpIndex("secondFrom")] && matchesInt[re.SubexpIndex("firstTo")] <= matchesInt[re.SubexpIndex("secondTo")] {
 			overlapCount++
 		}
 	}
 
 	return overlapCount
-}
-
-// Converts 4-8 into [4,5,6,7,8]
-func expandInts(start int, end int) []int {
-	output := make([]int, 0)
-	for i := start; i <= end; i++ {
-		output = append(output, i)
-	}
-	return output
 }
 
 func day4Part2(path string) int {
@@ -59,15 +49,12 @@ func day4Part2(path string) int {
 		matchesInt := utils.StringSliceToIntSlice(matches[1:]) // First match (the line) cannot be parsed
 		matchesInt = append([]int{-1}, matchesInt...)          // To make it easier I re-add the first element
 
-		first := expandInts(matchesInt[re.SubexpIndex("firstFrom")], matchesInt[re.SubexpIndex("firstTo")])
-		second := expandInts(matchesInt[re.SubexpIndex("secondFrom")], matchesInt[re.SubexpIndex("secondTo")])
-
 		// Don't find overlaps, just kick-out the things that DON'T overlap ;) (here we don't care about the total overlaps!)
-		if second[0] > first[len(first)-1] || second[len(second)-1] < first[0] {
+		if matchesInt[re.SubexpIndex("secondFrom")] > matchesInt[re.SubexpIndex("firstTo")] || matchesInt[re.SubexpIndex("secondTo")] < matchesInt[re.SubexpIndex("firstFrom")] {
 			continue
 		}
 
-		if first[0] > second[len(second)-1] || first[len(first)-1] < second[0] {
+		if matchesInt[re.SubexpIndex("firstFrom")] > matchesInt[re.SubexpIndex("secondTo")] || matchesInt[re.SubexpIndex("firstTo")] < matchesInt[re.SubexpIndex("secondFrom")] {
 			continue
 		}
 
