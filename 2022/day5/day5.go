bcba51e711730a3a85e877b94d1698ca51300f72
Matthijs van Schooten <matthijs@munisense.com>
2022-12-22 12:08:27 +0100

    WIP

--- 2022/day5/day5.go
+++ 2022/day5/day5.go
@@ -10,10 +10,10 @@ import (
 
 func main() {
 	var count int
-	count = day5Part1("day5/day5-test.txt")
-	fmt.Println(fmt.Sprintf("Part 1 Test Answer %d", count))
-	//count = day5Part1("day5/day5.txt")
-	//fmt.Println(fmt.Sprintf("Part 1 Answer %d", count))
+	//count = day5Part1("day5/day5-test.txt")
+	//fmt.Println(fmt.Sprintf("Part 1 Test Answer %d", count))
+	count = day5Part1("day5/day5.txt")
+	fmt.Println(fmt.Sprintf("Part 1 Answer %d", count))
 	//count = day5Part2("day5/day5-test.txt")
 	//fmt.Println(fmt.Sprintf("Part 2 Test Answer %d", count))
 	//count = day5Part2("day5/day5.txt")
@@ -40,7 +40,7 @@ func day5Part1(path string) int {
 	var regMove = regexp.MustCompile(`(?m)move (?P<move>\d+) from (?P<from>\d+) to (?P<to>\d+)`)
 	for _, line := range lines {
 		if parseMoves {
-			fmt.Println(line)
+			//fmt.Println(line)
 			matches := regMove.FindAllStringSubmatch(line, -1)
 			for _, match := range matches {
 				move := move{
@@ -49,7 +49,7 @@ func day5Part1(path string) int {
 					to:     utils.MustParseStringToInt(match[regMove.SubexpIndex("to")]),
 				}
 				moves = append(moves, move)
-				fmt.Println(move)
+				//fmt.Println(move)
 			}
 		}
 
@@ -65,24 +65,26 @@ func day5Part1(path string) int {
 			for i, match := range matches {
 				fmt.Println(match[0], "found at index", i)
 				token := match[0]
-				if token == "    " {
+				token = strings.Trim(token, "[] ")
+				if token == "" {
 					token = " "
 				} else {
-					token = strings.Trim(token, "[] ")
 					// So now we either have a " " or a letter
 					_, ok := stacks[i+1]
 					if !ok {
 						stacks[i+1] = deque.New[string]()
 					}
-					stacks[i+1].PushFront(token)
+					stacks[i+1].PushBack(token)
 				}
 			}
 		}
 	}
 	printStacks(stacks)
-
-	stacks = executeMove(stacks, moves[0])
-	printStacks(stacks)
+	//printStacks(stacks)
+	//printStacks(stacks)
+	//fmt.Println("aaa")
+	//stacks = executeMove(stacks, moves[0])
+	//printStacks(stacks)
 
 	_ = columnAmount
 	_ = moves
@@ -103,7 +105,7 @@ func executeMove(stacks map[int]*deque.Deque[string], move move) map[int]*deque.
 }
 
 func printStacks(input map[int]*deque.Deque[string]) {
-	input = utils.CopyMap(input)
+	//input = utils.CopyMap(input)
 	// First find highest
 	highest := 0
 	for _, crates := range input {
@@ -112,6 +114,10 @@ func printStacks(input map[int]*deque.Deque[string]) {
 		}
 	}
 	fmt.Println(fmt.Sprintf("highest: %d", highest))
+
+	for k := range input {
+		fmt.Println(k)
+	}
 
 	for i := 0; i < highest; i++ {
 		for j := 0; j < len(input); j++ {
@@ -123,12 +129,14 @@ func printStacks(input map[int]*deque.Deque[string]) {
 				continue
 			}
 
-			crate := crates.PopBack()
+			//for k := 0; k < crates.Len(); k++ {
+			crate := crates.At(i % (crates.Len() - i)) // our indexes are reversed to what I print/expect
 			if crate == " " || crate == "" {
 				fmt.Print("---")
 			} else {
 				fmt.Print("[" + crate + "]")
 			}
+			//}
 		}
 		fmt.Println("")
 	}
