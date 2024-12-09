38ceb0c4083b239f0b6f21f270b98d70af0fba21
Matthijs <Mattie112@users.noreply.github.com>
2024-12-09 20:54:50 +0100

    This should be it

--- 2024/day9/day9.go
+++ 2024/day9/day9.go
@@ -14,8 +14,6 @@ func main() {
 	start := time.Now()
 	fmt.Println(fmt.Sprintf("Part 1 Test Answer %d, took %s", Part1("day9/day9-test.txt"), time.Since(start).String()))
 	start = time.Now()
-	fmt.Println(fmt.Sprintf("Part 1 Test Answer %d, took %s", Part1("day9/day9-test2.txt"), time.Since(start).String()))
-	start = time.Now()
 	fmt.Println(fmt.Sprintf("Part 2 Test Answer %d, took %s", Part2("day9/day9-test.txt"), time.Since(start).String()))
 	start = time.Now()
 	fmt.Println(fmt.Sprintf("Part 1 Answer %d, took %s", Part1("day9/day9.txt"), time.Since(start).String()))
@@ -24,8 +22,9 @@ func main() {
 }
 
 type file struct {
-	ID   int
-	char string
+	ID     int
+	char   string
+	length int
 }
 
 func Part1(path string) int {
@@ -105,5 +104,130 @@ OUTER:
 }
 
 func Part2(path string) int {
-	return 0
+	lines := utils.ReadLines(path)
+	line := lines[0]
+	answer := 0
+
+	diskMap := strings.Split(line, "")
+	nonVisual := make([]file, 0) // Actual values we can use for our calculation
+
+	fileID := 0
+	for i := 0; i < len(diskMap); i++ {
+		length := utils.MustParseStringToInt(diskMap[i])
+		switch i % 2 {
+		case 0:
+			nonVisual = append(nonVisual, file{ID: fileID, char: strconv.Itoa(fileID % 10), length: length})
+			fileID++
+		case 1:
+			if length > 0 {
+				nonVisual = append(nonVisual, file{ID: -1, char: ".", length: length})
+			}
+		}
+	}
+
+	if log.GetLevel() == log.DebugLevel {
+		str := ""
+		for i := 0; i < len(nonVisual); i++ {
+			str += strings.Repeat(nonVisual[i].char, nonVisual[i].length)
+		}
+		log.Debugf("Visual: %s", str)
+	}
+
+	// Part 2 is "order decreasing file ID", this is a poor-mans solution for that
+	lastId := nonVisual[len(nonVisual)-1].ID + 1
+
+ENDLESS:
+	for {
+	OUTER:
+		for i := len(nonVisual) - 1; i >= 0; i-- {
+			fileAtTheEnd := nonVisual[i]
+
+			// Part 2 is "order decreasing file ID", this is a poor-mans solution for that
+			if fileAtTheEnd.ID != lastId-1 {
+				continue
+			}
+
+			// We are done if we reach file 0
+			if lastId < 1 {
+				break ENDLESS
+			}
+
+			if fileAtTheEnd.char == "." {
+				continue
+			}
+
+			// Now we have a file at the end try to find a place where we can put it
+			for j := 0; j < len(nonVisual); j++ {
+				spaceAtTheFront := nonVisual[j]
+				if spaceAtTheFront.char != "." {
+					continue
+				}
+
+				// We have not enough space available
+				if spaceAtTheFront.length < fileAtTheEnd.length {
+					continue
+				}
+
+				if j > i {
+					// We will not move a program to the back
+					lastId = fileAtTheEnd.ID
+					break OUTER
+				}
+
+				// We have exactly enough space available
+				if spaceAtTheFront.length == fileAtTheEnd.length {
+					// We found a place to put it
+					nonVisual[j] = fileAtTheEnd
+					nonVisual[i] = file{ID: -1, char: ".", length: fileAtTheEnd.length}
+				}
+
+				// We have more space available
+				if spaceAtTheFront.length > fileAtTheEnd.length {
+					nonVisual[j] = fileAtTheEnd
+					// After index j insert a new file with space
+					nonVisual = append(nonVisual[:j+1], append([]file{{ID: -1, char: ".", length: spaceAtTheFront.length - fileAtTheEnd.length}}, nonVisual[j+1:]...)...)
+					nonVisual[i+1] = file{ID: -1, char: ".", length: fileAtTheEnd.length}
+				}
+				lastId = fileAtTheEnd.ID
+				break OUTER
+			}
+
+			lastId = fileAtTheEnd.ID
+
+			if log.GetLevel() == log.DebugLevel {
+				str := ""
+				for i := 0; i < len(nonVisual); i++ {
+					str += strings.Repeat(nonVisual[i].char, nonVisual[i].length)
+				}
+				log.Debugf("Visual after sorting step %d: %s", i, str)
+			}
+		}
+	}
+
+	if log.GetLevel() == log.DebugLevel {
+		str := ""
+		for i := 0; i < len(nonVisual); i++ {
+			str += strings.Repeat(nonVisual[i].char, nonVisual[i].length)
+		}
+		log.Debugf("Visual after sorting: %s", str)
+	}
+
+	visual := make([]file, 0)
+
+	// Calculate the checksum (same way as part 1, first converting to a string
+	for i := 0; i < len(nonVisual); i++ {
+		for j := 0; j < nonVisual[i].length; j++ {
+			visual = append(visual, nonVisual[i])
+		}
+	}
+
+	for i := 0; i < len(visual); i++ {
+		f := visual[i]
+		if f.char == "." {
+			continue
+		}
+		answer += i * (f.ID)
+	}
+
+	return answer
 }
--- 2024/day9/day9_test.go
+++ 2024/day9/day9_test.go
@@ -11,8 +11,8 @@ func Test_day9Part1(t *testing.T) {
 		args args
 		want int
 	}{
-		{"part 1 test", args{path: "day9-test.txt"}, 3749},
-		{"part 1 answer", args{path: "day9.txt"}, 12839601725877},
+		{"part 1 test", args{path: "day9-test.txt"}, 1928},
+		{"part 1 answer", args{path: "day9.txt"}, 6330095022244},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -32,8 +32,8 @@ func Test_day9Part2(t *testing.T) {
 		args args
 		want int
 	}{
-		{"part 2 test", args{path: "day9-test.txt"}, 11387},
-		{"part 2 answer", args{path: "day9.txt"}, 149956401519484},
+		{"part 2 test", args{path: "day9-test.txt"}, 2858},
+		{"part 2 answer", args{path: "day9.txt"}, 6359491814941},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
