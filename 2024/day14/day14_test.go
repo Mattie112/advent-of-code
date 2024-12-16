3155c992b1a29edb844b3bceb11e5f2d4db92ddc
Matthijs <Mattie112@users.noreply.github.com>
2024-12-16 20:22:34 +0100

    done!

--- 2024/day14/day14_test.go
+++ 2024/day14/day14_test.go
@@ -4,19 +4,22 @@ import "testing"
 
 func Test_day14Part1(t *testing.T) {
 	type args struct {
-		path string
+		path    string
+		width   int
+		height  int
+		seconds int
 	}
 	tests := []struct {
 		name string
 		args args
 		want int
 	}{
-		{"part 1 test", args{path: "day14-test.txt"}, 55312},
-		{"part 1 answer", args{path: "day14.txt"}, 202019},
+		{"part 1 test", args{path: "day14-test.txt", width: 11, height: 7, seconds: 100}, 12},
+		{"part 1 answer", args{path: "day14.txt", width: 101, height: 103, seconds: 100}, 230436441},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Part1(tt.args.path, 25); got != tt.want {
+			if got := Part1(tt.args.path, tt.args.width, tt.args.height, tt.args.seconds); got != tt.want {
 				t.Errorf("day14Part1() = %v, want %v", got, tt.want)
 			}
 		})
@@ -25,19 +28,21 @@ func Test_day14Part1(t *testing.T) {
 
 func Test_day14Part2(t *testing.T) {
 	type args struct {
-		path string
+		path    string
+		width   int
+		height  int
+		seconds int
 	}
 	tests := []struct {
 		name string
 		args args
 		want int
 	}{
-		{"part 2 test", args{path: "day14-test.txt"}, 65601038650482},
-		{"part 2 answer", args{path: "day14.txt"}, 239321955280205},
+		{"part 2 answer", args{path: "day14.txt", width: 101, height: 103, seconds: 100}, 8270},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Part2(tt.args.path, 75); got != tt.want {
+			if got := Part2(tt.args.path, tt.args.width, tt.args.height, tt.args.seconds); got != tt.want {
 				t.Errorf("day14Part2() = %v, want %v", got, tt.want)
 			}
 		})
